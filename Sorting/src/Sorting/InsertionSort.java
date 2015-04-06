7f9d29cd31a64348be806f87131701b0a4051e1c
Colton Cookerly <colton_cookerly@brown.edu>
2015-04-06 18:34:13 -0400

    added insertion sort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -9,7 +9,17 @@ public class InsertionSort {
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
 		//TODO: S2 implements insertion sort
+		int[] sortedArray = Arrays.copyOf(unsortedArray);
+		for (int i = 1; i < sortedArray.length; i++) {
+			int j = i;
+			while ((j > 0) && (sortedArray[j - 1] > sortedArray[j]) {
+				int temp = sortedArray[j - 1];
+				sortedArray[j - 1] = sortedArray[j];
+				sortedArray[j] = temp;
+				j = j - 1;
+			}
+		}
 		
-		return null;
+		return sortedArray;
 	}
 }
